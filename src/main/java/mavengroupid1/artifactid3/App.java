9607d51dad72aed18e70cfefb6fad4a898a13176
rex <arockiarexy1993@gmail.com>
2024-07-02 12:52:04 +0530

    this is java developer 1st commit

---
+++ src/main/java/mavengroupid1/artifactid3/App.java
@@ -0,0 +1,13 @@
+package mavengroupid1.artifactid3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
